2bc52cc4e6d12cb7c40fb57b85470e27ff5583cc
abhi633 <62231158+abhi633@users.noreply.github.com>
2024-09-03 11:18:40 +0930

    Update SFConnect.cs

--- Services/SFConnect.cs
+++ Services/SFConnect.cs
@@ -13,7 +13,7 @@ namespace ArdantOffical.Services
     {   
         public static string User = "access@ardant.com.au";
         public static string Password = "cArl0905$$$$";
-        public static string Token = "3VwRdO5SZLa9xXsGrEDgJuCQ";
+        public static string Token = "R3y7G69dbiUr0ZEPINWk0kG6";
         const string sfdcConsumerKey = "3MVG9q4K8Dm94dAzDT_aDS743.P9QRZAcnaEAmyep1.iuO5yau7zk7oygLxR4XAtKu_5izSqVe7vvIE0nTbsB";
         const string sfdcConsumerSecret = "C7023AEEF43414D91F62472A996EC13BD5EDDC9F773ADB9CE741DBF007634206";
        
